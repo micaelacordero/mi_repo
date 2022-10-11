90551f4199bbe6313691581066c8c46c94d921b6
Micaela Cordero <micaela.cordero94@gmail.com>
2022-10-10 22:20:35 -0300

    Pre-Entrega 2

--- script.js
+++ script.js
@@ -1,131 +1,162 @@
 /* js productos*/
 
-let nombreProducto1 = "Bacha Artesanal"
-let precioProducto1 = 4850
-let stockProducto1 = 5
-
-let nombreProducto2 = "Jarrita Acqua"
-let precioProducto2 = 1200
-let stockProducto2 = 20
-
-let nombreProducto3 = "Jarrón Rubí"
-let precioProducto3 = 3300
-let stockProducto3 = 25
-
-let nombreProducto4 = "Cuenco Acqua"
-let precioProducto4 = 1300
-let stockProducto4 = 30
-
-let nombreProducto5 = "Taza rústica rubí"
-let precioProducto5 = 1500
-let stockProducto5 = 50
-
-let nombreProducto6 = "Hornito rubí"
-let precioProducto6 = 1900
-let stockProducto6 = 30
+let producto1 = {
+  nombre: "bacha artesanal",
+  precio: 4850,
+  stock: 5
+}
  
-let nombreProducto7 = "Ensaladera Mar"
-let precioProducto7 = 2800
-let stockProducto7 = 20
+let producto2 = {
+  nombre: "jarrita acqua",
+  precio: 1200,
+  stock: 20
+}
+ 
+let producto3 = {
+  nombre: "jarrón rubí",
+  precio: 3300,
+  stock: 25
+}
+ 
+let producto4 = {
+  nombre: "cuenco acqua",
+  precio: 1300,
+  stock: 30
+}
+ 
+let producto5 = {
+  nombre:"taza rústica rubí",
+  precio: 1500,
+  stock: 50  
+} 
 
-let nombreProducto8 = "Mate Acqua"
-let precioProducto8 = 1400
-let stockProducto8 = 70
+let producto6 = {
+  nombre: "hornito rubí",
+  precio: 1900,
+  stock: 30
+}
+  
+let producto7 = {
+  nombre: "ensaladera mar",
+  precio: 2800,
+  stock: 20
+}
+ 
+let producto8 = {
+  nombre: "mate acqua",
+  precio: 1400,
+  stock: 70
+}
+ 
 
 let precioTotal = 0
 
-alert ("Bienvenidos a Magia Blanca! Aquí podra adquirir los siguientes productos: \n - Bacha Artesanal  \n - Jarrita Acqua \n - Jarrón Rubí \n - Cuenco Acqua \n - Taza rústica rubí \n - Hornito rubí \n - Ensaladera Mar \n - Mate Acqua")
+let listaProductos = [producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8]
+
+let listaNombres = []
+
+for (const producto of listaProductos){
+     listaNombres.push(producto.nombre) 
+}
+
+function saludo (){
+     alert ("Gracias por ingresar a Magia Blanca!")
+}
+
+saludo();
+
+alert ("Aquí podra adquirir los siguientes productos: \n - " + listaNombres.join("\n - "))
 
 let cantidadCompra = prompt ("Cuantos productos desea adquirir?")
 
 for(let i = 0; i < cantidadCompra; i = i + 1){
-  let productoCompra = prompt("Que producto desea comprar?: \n - Bacha Artesanal \n - Jarrita Acqua \n - Jarrón Rubí \n - Cuenco Acqua \n - Taza rústica rubí \n - Hornito rubí \n - Ensaladera Mar \n - Mate Acqua")
+  let productoCompra = prompt("Que producto desea comprar?: \n - bacha artesanal \n - jarrita acqua \n - jarrón rubí \n - cuenco acqua \n - taza rústica rubí \n - hornito rubí \n - ensaladera mar \n - mate acqua")
 
-if (productoCompra == "Bacha Artesanal"){
+if (productoCompra == "bacha artesanal"){
   
-  let cantidadProductoBacha = prompt("Ingrese que cantidad de " + nombreProducto1 + " desea comprar:")
-  if (cantidadProductoBacha <= stockProducto1){
-       precioTotal = precioTotal + (cantidadProductoBacha * precioProducto1)
+  let cantidadProductoBacha = prompt("Ingrese que cantidad de " + producto1.nombre + " desea comprar:")
+  if (cantidadProductoBacha <= producto1.stock){
+       precioTotal = precioTotal + (cantidadProductoBacha * producto1.precio)
   }
   else {
-      alert("El stock actual de este producto es de: " + stockProducto1 + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
+      alert("El stock actual de este producto es de: " + producto1.stock + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
   }
 }
 
-else if (productoCompra == "Jarrita Acqua"){
+else if (productoCompra == "jarrita acqua"){
   
-  let cantidadProductoJarrita = prompt("Ingrese que cantidad de " + nombreProducto2 + " desea comprar:")
-  if (cantidadProductoJarrita <= stockProducto2){
-       precioTotal = precioTotal + (cantidadProductoJarrita * precioProducto2) 
+  let cantidadProductoJarrita = prompt("Ingrese que cantidad de " + producto2.nombre + " desea comprar:")
+  if (cantidadProductoJarrita <= producto2.stock){
+       precioTotal = precioTotal + (cantidadProductoJarrita * producto2.precio) 
   }
   else {
-    alert("El stock actual de este producto es de: " + stockProducto2 + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
+    alert("El stock actual de este producto es de: " + producto2.stock + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
 }
 }
 
-else if (productoCompra == "Jarrón Rubí"){
+else if (productoCompra == "jarrón rubí"){
   
-  let cantidadProductoJarrón = prompt("Ingrese que cantidad de " + nombreProducto3 + " desea comprar:")
-  if (cantidadProductoJarrón <= stockProducto3) {
-       precioTotal = precioTotal + (cantidadProductoJarrón * precioProducto3) 
+  let cantidadProductoJarrón = prompt("Ingrese que cantidad de " + producto3.nombre + " desea comprar:")
+  if (cantidadProductoJarrón <= producto3.stock) {
+       precioTotal = precioTotal + (cantidadProductoJarrón * producto3.precio) 
   }
   else {
-    alert("El stock actual de este producto es de: " + stockProducto3 + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
+    alert("El stock actual de este producto es de: " + producto3.stock + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
 } 
 }
 
-else if (productoCompra == "Cuenco Acqua"){
+else if (productoCompra == "cuenco acqua"){
   
-  let cantidadProductoCuenco = prompt("Ingrese que cantidad de " + nombreProducto4 + " desea comprar:")
-  if (cantidadProductoCuenco <= stockProducto4) {
-        precioTotal = precioTotal + (cantidadProductoCuenco * precioProducto4) 
+  let cantidadProductoCuenco = prompt("Ingrese que cantidad de " + producto4.nombre + " desea comprar:")
+  if (cantidadProductoCuenco <= producto4.stock) {
+        precioTotal = precioTotal + (cantidadProductoCuenco * producto4.precio) 
   }
   else {
-    alert("El stock actual de este producto es de: " + stockProducto4 + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
+    alert("El stock actual de este producto es de: " + producto4.stock + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
 }  
 }
 
-else if (productoCompra == "Taza rústica rubí"){
+else if (productoCompra == "taza rústica rubí"){
   
-  let cantidadProductoTaza = prompt("Ingrese que cantidad de " + nombreProducto5 + " desea comprar:")
-  if (cantidadProductoTaza <= stockProducto5) {
-       precioTotal = precioTotal + (cantidadProductoTaza * precioProducto5)
+  let cantidadProductoTaza = prompt("Ingrese que cantidad de " + producto5.nombre + " desea comprar:")
+  if (cantidadProductoTaza <= producto5.stock) {
+       precioTotal = precioTotal + (cantidadProductoTaza * producto5.precio)
   }
   else {
-    alert("El stock actual de este producto es de: " + stockProducto5 + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
+    alert("El stock actual de este producto es de: " + producto5.stock + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
 }  
 }
 
-else if (productoCompra == "Hornito rubí"){
+else if (productoCompra == "hornito rubí"){
   
-  let cantidadProductoHornito = prompt("Ingrese que cantidad de " + nombreProducto6 + " desea comprar:")
-  if (cantidadProductoHornito <= stockProducto6) {
-        precioTotal = precioTotal + (cantidadProductoHornito * precioProducto6)
+  let cantidadProductoHornito = prompt("Ingrese que cantidad de " + producto6.nombre + " desea comprar:")
+  if (cantidadProductoHornito <= producto6.stock) {
+        precioTotal = precioTotal + (cantidadProductoHornito * producto6.precio)
   }
   else {
-    alert("El stock actual de este producto es de: " + stockProducto6 + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
+    alert("El stock actual de este producto es de: " + producto6.stock + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
 }
 }
 
-else if (productoCompra == "Ensaladera Mar"){
+else if (productoCompra == "ensaladera mar"){
   
-  let cantidadProductoEnsaladera = prompt("Ingrese que cantidad de " + nombreProducto7 + " desea comprar:")
-  if (cantidadProductoEnsaladera <= stockProducto7) {
-        precioTotal = precioTotal + (cantidadProductoEnsaladera * precioProducto7) 
+  let cantidadProductoEnsaladera = prompt("Ingrese que cantidad de " + producto7.nombre + " desea comprar:")
+  if (cantidadProductoEnsaladera <= producto7.stock) {
+        precioTotal = precioTotal + (cantidadProductoEnsaladera * producto7.precio) 
   }
   else {
-    alert("El stock actual de este producto es de: " + stockProducto7 + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
+    alert("El stock actual de este producto es de: " + producto7.stock + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
 }
 }
 
-else if (productoCompra == "Mate Acqua"){
+else if (productoCompra == "mate acqua"){
   
-  let cantidadProductoMate = prompt("Ingrese que cantidad de " + nombreProducto8 + " desea comprar:")
-  if (cantidadProductoMate <= stockProducto8) {
-        precioTotal = precioTotal + (cantidadProductoMate * precioProducto8)
+  let cantidadProductoMate = prompt("Ingrese que cantidad de " + producto8.nombre + " desea comprar:")
+  if (cantidadProductoMate <= producto8.stock) {
+        precioTotal = precioTotal + (cantidadProductoMate * producto8.precio)
   }
   else {
-    alert("El stock actual de este producto es de: " + stockProducto8 + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
+    alert("El stock actual de este producto es de: " + producto8.stock + ". En caso de necesitar realizar un pedido mayorista por favor comunicarse a través de la página de contacto.")
   } 
 }
 } 
@@ -133,6 +164,8 @@ else if (productoCompra == "Mate Acqua"){
 if (precioTotal != 0){
   alert("El precio total es: " + precioTotal)
 }
+
+saludo();
 
 
 
